301ebb1c997e7c5b024ae8907455c4732ec65803
Alexander Beley <beleysasha@gmail.com>
2024-04-08 17:35:10 +1200

    leroy-parser added

--- leroy-parser.py
+++ leroy-parser.py
@@ -22,8 +22,8 @@ from bs4 import BeautifulSoup
 
 from pyvirtualdisplay import Display
 
-display = Display(visible=0, size=(1920, 1080))
-display.start()
+# display = Display(visible=0, size=(800, 600))
+# display.start()
 
 url = 'https://leroymerlin.ru/catalogue/tovary-dlya-uborki/'
 # url = 'https://leroymerlin.ru/catalogue/teploizolyaciya/'
@@ -37,6 +37,7 @@ options = Options()
 # options.add_experimental_option('useAutomationExtension', False)
 options.add_argument("--disable-blink-features=AutomationControlled")
 # options.add_argument('--headless')
+options.add_argument("--headless=chrome")
 # options.add_argument('--no-sandbox')
 prefs = {"profile.managed_default_content_settings.images": 2}
 options.add_experimental_option("prefs", prefs)
