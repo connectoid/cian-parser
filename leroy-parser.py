eed3c9c18dbad4e8c1146f5118577b20658cc3c9
Alexander Beley <beleysasha@gmail.com>
2024-04-09 12:26:14 +1200

    leroy-parser added

--- leroy-parser.py
+++ leroy-parser.py
@@ -34,16 +34,21 @@ script_name = 'window.INITIAL_STATE["plp"]'
 
 
 options = Options()
+
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36')
+
 # options.add_argument("--headless")
 # options.add_experimental_option("excludeSwitches", ["enable-automation"])
 # options.add_experimental_option('useAutomationExtension', False)
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument('--headless')
+# options.add_argument("--disable-blink-features=AutomationControlled")
+# options.add_argument('--headless')
 # options.add_argument("--headless=chrome")
 # options.add_argument('--disable-gpu')
 # options.add_argument('--window-size=1512,982')
-options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36')
-options.add_argument('--no-sandbox')
+# options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36')
+# options.add_argument('--no-sandbox')
 # prefs = {"profile.managed_default_content_settings.images": 2}
 # options.add_experimental_option("prefs", prefs)
 
